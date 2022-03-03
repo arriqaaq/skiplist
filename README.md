96c3d4c735f013a7859343ad0e0c9c558bd8485a
Farhan Khan <kfarhan@kfarhan-a01.vmware.com>
2022-03-03 13:03:35 +0530

    chore: add skiplist data structure

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # skiplist
 A Skiplist implementation in Go
+
+Getting Started
+===============
+
+## Installing
+
+To start using hash, install Go and run `go get`:
+
+```sh
+$ go get -u github.com/arriqaaq/skiplist
+```
+
+This will retrieve the library.
+
+## Usage
+
+```go
+package main
+
+import "github.com/arriqaaq/skiplist"
+
+type kv struct{k,v string}
+
+func main() {
+
+    // Set (accepts any value)
+    n := skiplist.New()
+	val := []byte("test_val")
+
+	n.Set("ec", val)
+	n.Set("dc", 123)
+	n.Set("ac", val)
+
+    // Get
+    node:= list.Get("ec")
+	assert.Equal(t, val, node.Value())
+
+    // Delete
+	n.Delete("dc")
+}
+```
+
+## Supported Commands
+
+```go
+Supported  commands
+
+Set
+Get
+Delete
+```
+
---
+++ skiplist_test.go
@@ -0,0 +1,78 @@
+package skiplist
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func makeList(n int) *Skiplist {
+	list := New()
+	for i := 1; i <= n; i++ {
+		key := fmt.Sprintf("key_%d", i)
+		member := fmt.Sprintf("member_%d", i)
+		list.Set(key, member)
+	}
+	return list
+}
+
+func TestSkipList_Get(t *testing.T) {
+	list := New()
+	val := []byte("test_val")
+
+	list.Set("ec", val)
+	list.Set("dc", 123)
+	list.Set("ac", val)
+
+	assert.Equal(t, val, list.Get("ec").Value())
+	assert.Equal(t, val, list.Get("ac").value)
+	assert.Equal(t, 123, list.Get("dc").value)
+}
+
+func TestSkipList_Skl(t *testing.T) {
+	list := makeList(10000)
+
+	// check list for items
+	for i := 1; i < 1000; i++ {
+		key := fmt.Sprintf("key_%d", i)
+		member := fmt.Sprintf("member_%d", i)
+		assert.Equal(t, member, list.Get(key).Value())
+	}
+
+	// delete list for items
+	for i := 500; i < 1000; i++ {
+		key := fmt.Sprintf("key_%d", i)
+		list.Delete(key)
+	}
+
+	// check list for items
+	for i := 1; i < 500; i++ {
+		key := fmt.Sprintf("key_%d", i)
+		member := fmt.Sprintf("member_%d", i)
+		assert.Equal(t, member, list.Get(key).Value())
+	}
+}
+
+func TestSkipList_Remove(t *testing.T) {
+	list := New()
+	val := []byte("test_val")
+
+	list.Set("ec", val)
+	list.Set("dc", 123)
+	list.Set("ac", val)
+
+	list.Delete("dc")
+	list.Delete("ec")
+	list.Delete("ac")
+
+	assert.Nil(t, list.Get("ec"))
+	assert.Nil(t, list.Get("ac"))
+	assert.Nil(t, list.Get("dc"))
+
+}
+
+func TestSkipList_Keys(t *testing.T) {
+	list := makeList(100)
+	assert.Equal(t, 100, len(list.Keys()))
+}
